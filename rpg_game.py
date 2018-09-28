c7ac4234e6ac4401b0fb0c3b5b0b6cf1b1b37939
Stanley Chen <dannyshien@gmail.com>
2018-09-28 09:13:58 -0400

    add attack and receive damaage for class character

--- rpg_game.py
+++ rpg_game.py
@@ -1,6 +1,7 @@
 #RPG GAME 1
 class Character: 
   def __init__(self, health, power):
+    self.name = ''
     self.health = health
     self.power = power
 
@@ -9,36 +10,47 @@ class Character:
       return True
    return False
 
+  def attack(self, enemy): 
+    if not self.status():
+      return
+    print('%s attacks %s' % (self.name, enemy.name))
+
+  def receive_damage(self, points):
+    self.health -=points
+    print('%s recieved % damage.' % (self.name, points))
+    if self.health <= 0: 
+      print('%s is dead. ' % (self.name))
+
   def print_status(self):
-    print('You have %d health and %d power.' % (self.health, self.power))
+    print('%s have %d health and %d power.' % (self.name, self.health, self.power))
 
 class Hero(Character):
   def __init__(self, health, power):
     self.name = 'Heo'
 
-  def attack(self, enemy):
-    enemy.health -= self.power
-    print('You do %d damamge to the Goblin.' % self.power)
+  # def attack(self, enemy):
+  #   enemy.health -= self.power
+  #   print('You do %d damamge to the Goblin.' % self.power)
 
-  def print_status(self):
-    print('You have %d health and %d power.' % (self.health, self.power))
+  # def print_status(self):
+  #   print('You have %d health and %d power.' % (self.health, self.power))
 
 class Goblin(Character):
   def __init__(self, health, power):
     self.name = 'Golin'
 
-  def attack(self, swordsman):
-    swordsman.health -= self.power
-    print('The Goblin does %d damage to you.' % self.power)
+  # def attack(self, swordsman):
+  #   swordsman.health -= self.power
+  #   print('The Goblin does %d damage to you.' % self.power)
 
-  def print_status(self): 
-    print('You have %d health and %d power.' % (self.health, self.power))
+  # def print_status(self): 
+  #   print('You have %d health and %d power.' % (self.health, self.power))
 
 class zombie(Character):
   def __init__(self, health, power):
     self.name = 'Walker'
 
-  def attack(self, dead):
+  def attack(self):
     #if swordsman.health > 6:
     if self.health > 6:
       #swordsman.health -= self.power
@@ -49,9 +61,8 @@ class zombie(Character):
     #if self.health > 0: 
       return True
 
-
-  def print_status(self):
-    print('You have %d health and %d power.' % (self.health, self.power))
+  # def print_status(self):
+  #   print('You have %d health and %d power.' % (self.health, self.power))
 
 
 # def begin():
